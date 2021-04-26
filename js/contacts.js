3c44155042132f3c43a1a8af3bb7cefc9ab993e7
diana3664 <dianamus2017@gmail.com>
2021-04-26 17:53:23 +0300

    change the scripts file

---
+++ js/contacts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $("form#form").submit(function(event){
+    var nameInput = $("input#name").val();
+    var emailInput = $("input#email").val();
+    var messageInput = $("textarea#message").val();
+    if ($("input#name").val() && $("input#email").val()){
+      alert (nameInput + ", Welcome to Delani studio.Our team of experts will get intouch with you within 24hours");
+    }
+    else {
+      alert("invalid email!");
+    }
+    
+  });
+
+});
---
+++ js/script.js
@@ -0,0 +1,16 @@
+// function Order(size, toppings, price, buy) {
+//   this.size = size;
+//   this.toppings = toppings;
+//   this.price = price;
+//   this.buy = buy;
+// }
+
+
+// // logic interface
+// $(document).ready(function() {
+//   $("button.btn0").click(function(event) {
+//     event.preventDefault();
+//     $("#value0").append(event)
+//   });
+// });
+
