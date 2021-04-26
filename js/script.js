38dbc48d95734814c77cf37aea988c19f3936f6a
diana3664 <dianamus2017@gmail.com>
2021-04-26 18:39:28 +0300

    add a script js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,8 @@
-// function Order(size, toppings, price, buy) {
-//   this.size = size;
-//   this.toppings = toppings;
-//   this.price = price;
-//   this.buy = buy;
-// }
-
-
-// // logic interface
-// $(document).ready(function() {
-//   $("button.btn0").click(function(event) {
-//     event.preventDefault();
-//     $("#value0").append(event)
-//   });
-// });
-
+function singlePizza (size, toppings, price ) {
+  this.size = size;
+  this.toppings = toppings;
+  this.price = price;
+}
+size = ["small", "medium", "large"]
+toppings =["beef","chicken","pineapple"]
+price = [550,650, 750,750,800,]
